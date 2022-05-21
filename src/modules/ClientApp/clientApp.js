60d19aaf4031d57b4e1030d81b866ad45bf37b96
deepsingh <deepsingh.cts@gmail.com>
2022-05-21 13:53:13 +0530

    counter for cart

--- src/modules/ClientApp/clientApp.js
+++ src/modules/ClientApp/clientApp.js
@@ -16,8 +16,37 @@ const inventory = [
         reviews: [{ reviewer: { id: '', name: '' }, rating: '', reviewDesc: '' }]
     }
 ]
+
+const cart = [];
 class ClientApp extends Component {
+    constructor() {
+        super()
+        this.state = {
+            cartValue: 0
+        }
+    }
+
+    onAdd = () => {
+        this.setState((prev) => {
+            return {
+                cartValue: prev.cartValue + 1
+            }
+        })
+    }
+    onRemove = () => {
+        const { cartValue } = this.state;
+        if (cartValue > 0) {
+            this.setState((prev) => {
+                return {
+                    cartValue: prev.cartValue - 1
+                }
+            })
+        }
+
+    }
+
     renderInventoryList = () => {
+        const { cartValue } = this.state;
         return inventory.map((item, index) => {
             return <div className='inventory-item'>
                 <div>
@@ -26,17 +55,22 @@ class ClientApp extends Component {
                 <div>
                     Price: Rs. {item.price}
                 </div>
-                <button>ADD</button>
-                <button>REMOVE</button>
+                <button onClick={this.onAdd}>ADD</button>
+                <button onClick={this.onRemove}>REMOVE</button>
             </div>
 
         })
     }
     render() {
+        const { cartValue } = this.state;
         return (
             <>
                 <div className='inventory-list-viewer'>
                     {this.renderInventoryList()}
+                </div>
+                <div className='cart'>
+                    <div> &#8371; CART</div>
+                    <div>{cartValue}</div>
                 </div>
             </>
         )
