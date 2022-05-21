dd6a557e2f1448a5fb3d19c6ef567edcfeef8093
deepsingh <deepsingh.cts@gmail.com>
2022-05-21 13:39:35 +0530

    added app heading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="main-app">
+      <header className='app-heading'>BuY Me SoMeThInG</header>
         <ClientApp />
     </div>
   );
